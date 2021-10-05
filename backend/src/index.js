da79595427506d0a1dc38af7f0b3183a1c76af6b
Gabriel Cabral Toñanez <cabraltonanez@gmail.com>
2021-10-05 01:51:49 -0300

    backend terminado

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 const app = require('./app');
 require('./database');
 
-const main = async () => {
-    await app.listen(app.get('port'));
+const main = () => {
+    app.listen(app.get('port'));
     console.log('Server on port', app.get('port'));
 }
 
--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,13 +1,13 @@
 const { Schema, model } = require('mongoose');
 
-const userSchema = new Schema(
-    {
-        username: {
-            type: String,
-            required: true,
-            unique: true,
-            trim: true
-        }
+const userSchema = new Schema({
+    
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    }
     }, {
         timestamps: true
     });
